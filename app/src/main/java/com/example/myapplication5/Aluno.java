f18e6c325d29d38c11f8c9bdde9489999df1a3f9
FIXER3600 <guilhermecomercial51@gmail.com>
2019-09-09 22:01:22 -0300

    dvsdfvdf

---
+++ app/src/main/java/com/example/myapplication5/Aluno.java
@@ -0,0 +1,18 @@
+package com.example.myapplication5;
+
+public class Aluno {
+    private String nome;
+    private Integer idade;
+    public String getNome() {
+        return nome;
+    }
+    public Integer getIdade() {
+        return idade;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+}
